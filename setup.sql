e841d05ea4c9cb4611324c89f8968b3fc5853936
afheisleycook <101531588+afheisleycook@users.noreply.github.com>
2022-03-13 16:37:29 -0700

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -1,1 +1,5 @@
-create database 
+-- sets up db for log -- 
+create database dailyblog;
+create user dailyblog;
+grant all to "dailblog"  at dailyblog.*;
+create table posts(post_id integery primary key autoincrement,post_title varchar(30),post_description varchar(100), post_tag varchar(10) );
