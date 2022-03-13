21f23a5a0d615e8fc9ca0afd4be31ea2fc9b7659
afheisleycook <101531588+afheisleycook@users.noreply.github.com>
2022-03-13 16:45:21 -0700

    Update runapp.cpp

--- runapp.cpp
+++ runapp.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 int main()
 {
-system("python3 .");
+  char yorn;
+  std::cout << "instealled";
+  std::cin >> yorn;
+  cout << yorn;
+  if(yorn == 'y') {
+system("python3 __main__.py");
+  };
+  if(yorn == 'n') {
+    system("bash install.sh");
+  }
 }
